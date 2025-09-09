ad399d75743ea30575578ebbb3bb08810521d990
sammisam8888 <samuelpriyatam@gmail.com>
2025-09-09 17:50:52 +0530

    intial commit - 1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function greet() {
-    console.log("Hello from MAIN branch!");
-    
+    console.log("Hello from FEATURE branch!");
+    console.log("This is extra code in FEATURE.");
 }
 
 greet();
